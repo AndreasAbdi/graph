58b36d85087cd124ea3487a415f4fc24ef48ea7c
aabdi <andreasabdi@yahoo.ca>
2016-02-06 22:35:56 -0800

    initial commit

---
+++ src/main/java/graph/Greeter.java
@@ -0,0 +1,7 @@
+package main.java.graph;
+
+public class Greeter {
+    public String sayHello() {
+        return "Hello world";
+    }
+}
---
+++ test/main/java/graph/GreeterTest.java
@@ -0,0 +1,15 @@
+package main.java.graph;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class GreeterTest {
+
+    @Test
+    public void testSayHello() throws Exception {
+        Greeter greeter = new Greeter();
+        assertNotNull(greeter.sayHello());
+    }
+}
